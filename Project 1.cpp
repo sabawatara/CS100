385351188e6c0a15c5d9e85d8007e7761773b921
sabawatara <nijhum08@yahoo.com>
2017-01-27 18:02:20 +0600

    Add files via upload

---
+++ Project 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    string name;
+    cout<<"Hello there! Who are you?: ";
+    cin>>name;
+    cout<<"Oh, hey "<<name<<"!";
+    string university;
+    cout<< "Which university are you from?: ";
+    cin>>university;
+    cout<<"That's a pretty good university," << university<<"!";
+
+    return 0;
+}
