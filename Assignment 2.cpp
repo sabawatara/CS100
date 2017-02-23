75da6ede5e4ec4a90c5f07504d7afc202e07fbfe
sabawatara <nijhum08@yahoo.com>
2017-02-23 22:58:22 +0600

    Add files via upload

--- Assignment 2.cpp
+++ Assignment 2.cpp
@@ -4,18 +4,53 @@ using namespace std;
 
 int main()
 {
-    cout<< "This program converts meter into feet"<<endl;
+    cout<< "This is a simple meter-feet converter, accurate upto 4 d.p"<<endl;
     cout<< ""<<endl;
 
-    int length;
-    cout<< "Enter a value of length in meter"<<endl;
+    int proclaim;
 
-    float meter;
-    cin>> meter;
+    float mf1, fm1, mf2, fm2;
+    cout<< "Enter 1 to convert from meter to feet "<<endl;
+    cout<< "Enter 2 to convert from feet to meter "<<endl;
+    cout<< ""<<endl;
+    cout<< "Your number "<<endl;
+    cin>> proclaim;
 
-    float feet = meter * 3.28084;
+    if (proclaim == 1)
+    {
+        cout<< "You have chosen to convert from meter to feet."<<endl;
+        cout<< "Please enter your value in meters: "<<endl;
+        cin>> mf1;
+        fm1= mf1 * 3.2808;
 
-    cout<< "Length in feet = " <<feet<< endl;
+        cout<< "Converting "<<mf1<< " meters to feet"<<endl;
+        cout<< mf1<< " meters = " <<fm1<< " feet."<<endl;
+        cout<< ""<<endl;
+
+    }
+
+    else if (proclaim ==2)
+    {
+        cout<< "You have chosen to convert from feet to meter."<<endl;
+        cout<< "Please enter your value in feet: "<<endl;
+        cin>> fm2;
+        mf2 = fm2 * 3.2808;
+
+        cout<< "Converting " <<fm2<< " feet to meters."<<endl;
+        cout<< fm2<< " feet = " <<mf2<< " meter."<<endl;
+        cout<< ""<<endl;
+    }
+
+    else
+    {
+        cout<< "You have chosen an invalid input."<<endl;
+    }
+
+    cout<< "Program will now terminate."<<endl;
+    cout<< "Restart program if you would like to try again."<<endl;
 
     return 0;
 }
+
+
+
