15a69c8a83d478e915e2d70ec681805476d997a8
sabawatara <nijhum08@yahoo.com>
2017-02-25 04:55:33 +0600

    Add files via upload

--- Assignment4.cpp
+++ Assignment4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout<< "This program will calculate the area of a rectangle from its length and width."<<endl;
+    cout<< "Description : This program will calculate the area of a rectangle from its length and width."<<endl;
     cout<< ""<<endl;
 
     double length, width, area;
@@ -17,25 +17,32 @@ int main()
 
     area = length * width;
 
-    if (length <0, width <0)
+    if (length<0)
     {
         cout<< "Sorry your input is invalid. Please try again with positive values."<<endl;
-    }
-
-    else if (length >0, width >0)
-    {
-        cout<< "The area of the rectangle is " <<area<< "."<<endl;
-    }
-
-    if  (length = width)
-    {
-        cout<< "That's actually a square, not a rectangle. Anyways the area is " <<area<< "."<<endl;
         cout<< ""<<endl;
     }
+
+    if (width<0)
+    {
+        cout<< "Sorry your input is invalid. Please try again with positive values."<<endl;
+        cout<< ""<<endl;
+    }
+    else if (length == width)
+    {
+        cout<< "That's actually a square, not a rectangle. Anyways the area is "<<area<< "."<<endl;
+        cout<< ""<<endl;
+    }
+
+
+else if (length >0)
+{
+    cout<< "The area of the rectangle is " <<area<< "."<<endl;
+    cout<< ""<<endl;
+}
 
     cout<< "The program will now terminate."<<endl;
     cout<< "Restart program to try again. Thank you"<<endl;
 
     return 0;
 }
-
